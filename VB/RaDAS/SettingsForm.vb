8ad181ee4f7a957042ebacf19951f20554b6193b
biosan <biosan222@gmail.com>
2014-05-06 23:53:20 +0200

    Small bugfix
    Fixed bug in settings form.

--- VB/RaDAS/SettingsForm.vb
+++ VB/RaDAS/SettingsForm.vb
@@ -1,19 +1,22 @@
 ﻿Public Class SettingsForm
 
-    Private Sub SettingsForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    Dim flag As Boolean = False
+
+    Private Sub SettingsForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load, MyBase.Shown
 
         NumericUpDown1.Value = decimalDigits
         NumericUpDown2.Value = railLenght
 
     End Sub
 
-    Private Sub decimalDigitsChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown1.ValueChanged
+    Private Sub decimalDigitsChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown1.Validated, NumericUpDown1.MouseCaptureChanged, NumericUpDown1.Leave, NumericUpDown1.Enter, NumericUpDown1.DoubleClick, NumericUpDown1.Click
 
         decimalDigits = NumericUpDown1.Value
         DataForm.writeData()
 
     End Sub
-    Private Sub railLenghtChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown2.ValueChanged
+
+    Private Sub railLenghtChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown2.Validated, NumericUpDown2.MouseCaptureChanged, NumericUpDown2.Leave, NumericUpDown2.Enter, NumericUpDown2.DoubleClick, NumericUpDown2.Click
 
         railLenght = NumericUpDown2.Value
         For i = 1 To numberOfIntervals
